48ccea90b5cfccdd2042194faa3a517822a81227
Mykhaylo <test@gmail.com>
2026-05-11 17:56:35 +0300

    fifth upd

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+	base: '/Ceramic_soul/',
 	build: {
 		rollupOptions: {
 			input: {
