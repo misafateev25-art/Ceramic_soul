ab9ee1b7d045a9aff06bbc05a97f3c33abdc3f32
Mykhaylo <test@gmail.com>
2026-05-20 18:52:43 +0300

    big upd 2.0

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,11 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import "/src/sass/style.scss";
 
-let text = "fuck"
-const pnum = 3.14
-const isOpen = true
-
-let a
-let b = 'niger'
-
-console.log(a, b)
-console.log(text)
-console.log(pnum)
-
-const person = {
-    name: "Mc Lovin",
-    age: 19
-}
-
-const titles = ["Make your dream come true or decorate your home", "create or buy", "our store"]
-
-function calc(a, b) {
-    console.log(a + b);
-}
-
-calc(34, 33)
-
-if (isOpen) {
-    console.log('The shop is open, u can buy marijuana')
-} else {
-    console.log('Go to home Mc Lovin');
-}
-
-const vase = document.querySelector('.touch__label')
-console.log(vase);
-
-vase.addEventListener('click', () => {
-    console.log(vase);
-})
-
-// Чтобы работали стили на Github нужно ставить точку перед /src/sass...
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+});
